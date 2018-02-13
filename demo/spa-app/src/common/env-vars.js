ff05903323e629dcce0c6393c9f616f9a60483d9
Robbert Kauffman <robbert.kauffman@bloomreach.com>
2018-02-13 13:57:02 -0600

    updated and restructured environment variables

--- demo/spa-app/src/common/env-vars.js
+++ demo/spa-app/src/common/env-vars.js
@@ -1,22 +1,15 @@
 // only change these
-const cmsBaseUrl = 'http://localhost:8080';
+const scheme = 'http';
+const cmsHostName = 'localhost';
+const cmsPort = '8080';
 const cmsContextPath = '/site';
 const cmsBinariesPath = '/binaries';
-const cmsApiPath = '/resourceapi'
-const socketsServer = {
-  hostname: 'localhost',
-  port: '3123'
-}
+const cmsApiPath = '/resourceapi';
 
 // don't change these
-const baseUrls = {
-  cmsBaseUrl: cmsBaseUrl,
-  cmsBaseImageUrl: cmsBaseUrl + cmsContextPath + cmsBinariesPath,
-  cmsApiPath: cmsApiPath,
-  socketsServer: socketsServer,
-  socketsServerUrl: 'http://' + socketsServer.hostname + ':' + socketsServer.port
-};
+const baseUrls = {};
+baseUrls.cmsBaseUrl = cmsPort !== '' ? `${ scheme }://${ cmsHostName }:${ cmsPort }` : `${ scheme }://${ cmsHostName }`;
+baseUrls.cmsBaseImageUrl = `${ baseUrls.cmsBaseUrl}${ cmsContextPath }${ cmsBinariesPath }`;
+baseUrls.cmsApiPath = cmsApiPath;
 
-// use module.exports instead of export so this module can be imported by nodeJs server (server.js) which doesn't
-// support typescript
-module.exports.baseUrls = baseUrls;
+export { baseUrls };
