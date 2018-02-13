6c58c1402d9703b3dfbbf447881be30e65f88e76
Robbert Kauffman <robbert.kauffman@bloomreach.com>
2018-02-13 14:13:28 -0600

    moved CmsPage component to its own class, moved some global helper functions to their own file

---
+++ demo/spa-app/src/cms-components/page.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import { fetchCmsPage, fetchComponentUpdate } from '../common/fetch';
+import { cmsJavascriptInitialization } from '../common/cms-js-overrides';
+import { findChildById } from '../common/find-child-by-id';
+import CmsContainer from './container';
+
+export default class CmsPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  updateState (component, propertiesMap) {
+    // only update when a component changes, when propertiesMap is empty the user has clicked cancel in component settings
+    // refresh in that case as an easy workaround
+    if (Object.keys(propertiesMap).length === 0) {
+      window.location.reload();
+    } else {
+      if (component && component.metaData && component.metaData.refNS) {
+        const componentId = component.metaData.refNS;
+        // find the component that needs to be updated in the page structure object using its ID
+        const componentToUpdate = findChildById(this.state.pageStructure, componentId);
+        if (componentToUpdate !== undefined) {
+          // fetch updated component from the API
+          fetchComponentUpdate(this.props.match.params, componentId, propertiesMap).then(response => {
+            // API can return empty response when component is deleted
+            if (response) {
+              // API can return either a single component or single container
+              if (response.component) {
+                componentToUpdate.parent[componentToUpdate.idx] = response.component;
+              } else if (response.container) {
+                componentToUpdate.parent[componentToUpdate.idx] = response.container;
+              }
+              // update documents by merging with original documents map
+              if (response.documents) {
+                let documents = this.state.pageStructure.documents; // eslint-disable-line
+                // ignore error on next line, as variable is a reference to a sub-object of pageStructure
+                // and will be used when pageStructure is updated/set
+                documents = Object.assign(documents, response.documents);
+              }
+              // update the page structure after the component/container has been updated
+              this.setState({
+                pageStructure: this.state.pageStructure
+              });
+            }
+          });
+        } else {
+          console.log('Error! Component with id %s not found', componentId);
+        }
+      }
+    }
+  }
+
+  componentDidUpdate () {
+    // parse CMS comments for rendering of content & component overlays
+    // do this after client-side rendering is finished
+    // also, override Hippo Channel Manager functions for handling state changes of components & containers
+    cmsJavascriptInitialization(this);
+  }
+
+  componentDidMount() {
+    fetchCmsPage(this.props.match.params).then(data => {
+      this.setState({
+        pageStructure: data
+      });
+    });
+  }
+
+  render() {
+    const pageStructure = this.state.pageStructure;
+    const preview = this.props.match.params.preview;
+
+    let containers = null;
+    if (pageStructure && pageStructure.containers) {
+      const documents = pageStructure.documents;
+      containers = pageStructure.containers.map(container => {
+        return (<CmsContainer configuration={container} documents={documents} preview={preview} key={container.id}/>);
+      });
+    }
+
+    return (
+      <div>
+        { pageStructure &&
+        containers
+        }
+      </div>
+    );
+  }
+}
