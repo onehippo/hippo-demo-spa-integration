101378b1b4d6028a2c7409f2b1dd965f7e0fb781
Robbert Kauffman <robbert.kauffman@bloomreach.com>
2018-03-20 16:24:51 -0500

    HSTTWO-4274: fixed links for news list items after API link changes

--- app-examples/react/src/cms-components/essentials/news-item.js
+++ app-examples/react/src/cms-components/essentials/news-item.js
@@ -3,24 +3,26 @@ import { Link } from 'react-router-dom'
 import { parseDate } from "../../utils/date";
 
 export default class NewsItem extends React.Component {
+  renderLink (content) {
+    if (content._links && content._links.site && content._links.site.href) {
+      if (content._links.site.type === 'internal') {
+        return (<Link to={content._links.site.href}>{content.title}</Link>);
+      } else {
+        return (<a href={content._links.site.href}>{content.title}</a>)
+      }
+    }
+    return null;
+  }
+
   render () {
     const content = this.props.content;
     const manageContentButton = this.props.manageContentButton;
-
-    let link;
-    if (content._links && content._links.site) {
-      if (content._links.type === 'internal') {
-        link = <Link to={content._links.site}>{content.title}</Link>
-      } else {
-        link = <a href={content._links.site}>{content.title}</a>
-      }
-    }
 
     return (
       <div className="blog-post has-edit-button">
         { manageContentButton && manageContentButton }
         <h2 className="blog-post-title">
-          { link && link }
+          { this.renderLink(content) }
         </h2>
         <p className="blog-post-meta">
           { content.date &&
