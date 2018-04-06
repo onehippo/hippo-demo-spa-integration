05ae1a1fe85ae7f0c510b36580f73f888d117e9c
Mathijs den Burger <mathijs.denburger@bloomreach.com>
2018-04-06 14:44:40 +0200

    CHANNELMGR-1705 Let SPA.renderComponent re-render components

--- app-examples/react/src/cms-components/core/page.js
+++ app-examples/react/src/cms-components/core/page.js
@@ -14,46 +14,44 @@ export default class CmsPage extends React.Component {
     window.SPA = {
       init: (cms) => {
         this.cms = cms;
+      },
+      renderComponent: (id, propertiesMap) => {
+        this.updateState(id, propertiesMap);
       }
     }
   }
 
-  updateState (component, propertiesMap) {
+  updateState (componentId, propertiesMap) {
     // only update when a component changes, when propertiesMap is empty the user has clicked cancel in component settings
     // refresh in that case as an easy workaround
     if (Object.keys(propertiesMap).length === 0) {
       window.location.reload();
     } else {
-      if (component && component.metaData && component.metaData.refNS) {
-        const componentId = component.metaData.refNS;
-        // find the component that needs to be updated in the page structure object using its ID
-        const componentToUpdate = findChildById(this.state.pageModel, componentId);
-        if (componentToUpdate !== undefined) {
-          // fetch updated component from the API
-          fetchComponentUpdate(this.props.pathInfo, this.props.preview, componentId, propertiesMap).then(response => {
-            // API can return empty response when component is deleted
-            if (response) {
-              // API can return either a single component or single container
-              if (response.page) {
-                console.log(response.page);
-                componentToUpdate.parent[componentToUpdate.idx] = response.page;
-              }
-              // update documents by merging with original documents map
-              if (response.content) {
-                let content = this.state.pageModel.content; // eslint-disable-line
-                // ignore error on next line, as variable is a reference to a sub-object of pageModel
-                // and will be used when pageModel is updated/set
-                content = Object.assign(content, response.content);
-              }
-              // update the page structure after the component/container has been updated
-              this.setState({
-                pageModel: this.state.pageModel
-              });
+      // find the component that needs to be updated in the page structure object using its ID
+      const componentToUpdate = findChildById(this.state.pageModel, componentId);
+      if (componentToUpdate !== undefined) {
+        // fetch updated component from the API
+        fetchComponentUpdate(this.props.pathInfo, this.props.preview, componentId, propertiesMap).then(response => {
+          // API can return empty response when component is deleted
+          if (response) {
+            // API can return either a single component or single container
+            if (response.page) {
+              console.log(response.page);
+              componentToUpdate.parent[componentToUpdate.idx] = response.page;
             }
-          });
-        } else {
-          console.log('Error! Component with id %s not found', componentId);
-        }
+            // update documents by merging with original documents map
+            if (response.content) {
+              let content = this.state.pageModel.content; // eslint-disable-line
+              // ignore error on next line, as variable is a reference to a sub-object of pageModel
+              // and will be used when pageModel is updated/set
+              content = Object.assign(content, response.content);
+            }
+            // update the page structure after the component/container has been updated
+            this.setState({
+              pageModel: this.state.pageModel
+            });
+          }
+        });
       }
     }
   }
