c2f7edc19f0ff501405013e9cf11067dca013c42
Woonsan Ko <woonsan.ko@bloomreach.com>
2018-04-12 13:22:41 -0400

    Detecting hostname and port in browser embed modes

--- app-examples/react/src/env-vars.js
+++ app-examples/react/src/env-vars.js
@@ -1,12 +1,18 @@
 // only change these
 const scheme = 'http';
-const cmsHostName = '127.0.0.1';
-const cmsPort = '9080';
 const cmsContextPath = 'site';
 const cmsChannelPath = 'spa-hap';
 const cmsPreviewPrefix = '_cmsinternal';
 const cmsApiPath = 'resourceapi';
 const cmsApiComponentRenderingUrlSuffix = '?_hn:type=component-rendering&_hn:ref=';
+
+// detect the host automatically.
+let cmsHostName = '127.0.0.1';
+let cmsPort = '9080';
+if (typeof window !== 'undefined' && window.location.host !== "localhost:3000") {
+  cmsHostName = window.location.hostname;
+  cmsPort = window.location.port;
+}
 
 // don't change these
 const baseUrls = {};
